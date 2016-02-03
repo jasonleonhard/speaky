b1cf8e8eba2a0fb75b5cf16bafc92f336684289e
un5t0ppab13 <devbrights@gmail.com>
2016-02-03 09:33:24 -0800

    view rooms/show & partial _message output message.content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
-  root 'rooms#show'
+  # get 'rooms/show'
+  # root 'rooms#show'
+  root to: 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
